75aa588872478aa5c584e227e30d449ed5c0e8fb
Stephany Campos <stephanyenne@gmail.com>
2025-08-11 18:04:55 -0300

    Add files via upload

---
+++ src/teste.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(3, 4));
+console.log(saudacao("Stephany"));
+//# sourceMappingURL=teste.js.map
---
+++ src/teste.ts
@@ -0,0 +1,10 @@
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+console.log(multiplicar(3, 4));
+console.log(saudacao("Stephany"));
